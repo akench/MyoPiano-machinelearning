42edc72afc4153b282453b4c87b03087b561ebc0
akench <atharva.kench@gmail.com>
2018-04-07 19:24:46 -0400

    pb file added

--- train.py
+++ train.py
@@ -83,7 +83,7 @@ def model(net, keep_prob):
 
 def train():
 
-	data_util = DataUtil('processed_data', batch_size = 128, num_epochs = 7)
+	data_util = DataUtil('processed_data', batch_size = 128, num_epochs = 10)
 
 
 
